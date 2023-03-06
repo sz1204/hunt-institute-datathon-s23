e8894096f633a69740d1b779a159e8ad139147eb
sarah <sz1204@ad.unc.edu>
2023-03-06 14:16:24 -0500

    read data files from pandas

--- homepage.py
+++ homepage.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
+
+data15 = pd.read_csv("cleaned-data/data_2015.csv")
+data16 = pd.read_csv("cleaned-data/data_2016.csv")
+data17 = pd.read_csv("cleaned-data/data_2017.csv")
+data18 = pd.read_csv("cleaned-data/data_2018.csv")
+data19 = pd.read_csv("cleaned-data/data_2019.csv")
+data20 = pd.read_csv("cleaned-data/data_2020.csv")
+data21 = pd.read_csv("cleaned-data/data_2021.csv")
+data22 = pd.read_csv("cleaned-data/data_2022.csv")
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Homepage", page_icon=":books:", layout="wide")
