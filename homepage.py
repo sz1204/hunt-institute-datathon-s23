a6df90f2d0e734bc072dec4340f503854edd07fb
Rebecca Rozansky <32803510+rebeccarozansky@users.noreply.github.com>
2023-03-06 14:50:31 -0500

    added mkae graph

--- homepage.py
+++ homepage.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
+
+data15 = pd.read_csv("cleaned-data/data_2015.csv")
+data16 = pd.read_csv("cleaned-data/data_2016.csv")
+data17 = pd.read_csv("cleaned-data/data_2017.csv")
+data18 = pd.read_csv("cleaned-data/data_2018.csv")
+data19 = pd.read_csv("cleaned-data/data_2019.csv")
+data20 = pd.read_csv("cleaned-data/data_2020.csv")
+data21 = pd.read_csv("cleaned-data/data_2021.csv")
+data22 = pd.read_csv("cleaned-data/data_2022.csv")
+
+rural_counties = ["Cherokee", "Graham", "Clay", "Polk", "Yancey", "Alleghany", "Caswell", "Warren", "Greene", "Bladen", "Hyde", "Tyrrell"]
+mixed_rural_counties = ['Alexander', 'Anson', 'Ashe', 'Avery', 'Beaufort', 'Bertie', 'Brunswick', 'Burke', 'Caldwell', 'Camden', 'Carteret', 'Chatham', 'Chowan', 'Cleveland', 'Columbus', 'Craven', 'Currituck', 'Dare', 'Davidson', 'Davie', 'Duplin', 'Edgecombe', 'Franklin', 'Gates', 'Granville', 'Halifax', 'Harnett', 'Haywood', 'Henderson', 'Hertford', 'Hoke', 'Iredell', 'Jackson', 'Johnston', 'Jones', 'Lee', 'Lenoir', 'Lincoln', 'Macon', 'Madison', 'Martin', 'McDowell', 'Mitchell', 'Montgomery', 'Moore', 'Nash', 'Northampton', 'Onslow', 'Pamlico', 'Pasquotank', 'Pender', 'Perquimans', 'Person', 'Pitt', 'Randolph', 'Richmond', 'Robeson', 'Rockingham', 'Rowan', 'Rutherford', 'Sampson', 'Scotland', 'Stanly', 'Stokes', 'Surry', 'Swain', 'Transylvania', 'Union', 'Vance', 'Washington', 'Watauga', 'Wayne', 'Wilkes', 'Wilson', 'Yadkin']
+mixed_urban_counties = ["Buncombe", "Gaston", "Cabarrus", "Catawba", "Guilford", "Alamance", "Orange", "Cumberland"]
+urban_counties = ["Mecklenburg", "Forsyth", "Durham", "Wake", "New Hanover"]
+
+density_categories = {
+    'Rural': rural_counties,
+    'Mixed Rural': mixed_rural_counties,
+    'Mixed Urban': mixed_urban_counties,
+    'Urban': urban_counties
+}
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Homepage", page_icon=":books:", layout="wide")
@@ -12,14 +35,27 @@ st.markdown('   ')
 
 # Sidebar
 st.sidebar.title("Please Filter Here")
-# demographic = st.sidebar.radio() # Filter side bars through urban-rural density
-#density = st.sidebar.multiselect(
-#    "Select the Density:",
-#    options=df[""].unique()
-#)
+selected_densities = st.sidebar.multiselect('Select population densities', list(density_categories.keys()))
+mask = data15['County'].isin(sum([density_categories[d] for d in selected_densities], []))
+filtered_data = data15[mask]
 # health = st.sidebar.radio() # Filter side bars through type of healthcare - physical or mental
 
 
+
+# Creating bar charts for graduation rate per county
+df = data15[['County', 'Graduation Rate', '% Severe Housing Problems']]
+bar_chart = alt.Chart(df).mark_bar(color='primary_color').encode(
+    x='County',
+    y='Graduation Rate'
+)
+# Create a line chart for % Rural using Altair
+line_chart = alt.Chart(df).mark_line(color='secondary_color').encode(
+    x='County',
+    y='% Severe Housing Problems'
+)
+# Overlay the two charts
+chart = (bar_chart + line_chart)
+st.altair_chart(chart, use_container_width=True)
 
 
 primary_color = "#bf293c"
