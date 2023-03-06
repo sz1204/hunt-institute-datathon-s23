b2c2dcb969162180b3b390a1a9c280d3bef84ae2
sarah <sz1204@ad.unc.edu>
2023-03-06 11:32:04 -0500

    added logo

---
+++ homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Homepage", page_icon="assets/hunt-institute.jpg", layout="wide")
+
+st.image("assets/hunt-institute.jpg", width=100)
+
+st.title("Welcome to the Steminists' Hunt Institute Datathon 2023 dashboard!")
+st.markdown("Our project focused on measuring how certain legislation affecting affordable housing and youth mental health impacted student physical health, mental health, and graduation.")
+st.markdown('   ')
