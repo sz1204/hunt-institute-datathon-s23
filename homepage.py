c260e5afcba49e461461eb500d4554703daa0d41
sarah <sz1204@ad.unc.edu>
2023-03-06 15:08:03 -0500

    remove matplotlib

--- homepage.py
+++ homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 data15 = pd.read_csv("cleaned-data/data_2015.csv")
 data16 = pd.read_csv("cleaned-data/data_2016.csv")
