37e69edd2daccf595329e905817cc07764ca84ad
sarah <sz1204@ad.unc.edu>
2023-03-06 14:50:02 -0500

    testing alternative to selecting year

--- homepage.py
+++ homepage.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import matplotlib.pyplot as plt
 
 data15 = pd.read_csv("cleaned-data/data_2015.csv")
 data16 = pd.read_csv("cleaned-data/data_2016.csv")
@@ -40,22 +41,21 @@ mask = data15['County'].isin(sum([density_categories[d] for d in selected_densit
 filtered_data = data15[mask]
 # health = st.sidebar.radio() # Filter side bars through type of healthcare - physical or mental
 
+files = ["data{}.csv".format(year) for year in range(2015, 2023)]
 
+# create a sidebar select to choose the year
+selected_year = st.sidebar.selectbox("Select a year", range(2015, 2023))
 
-# Creating bar charts for graduation rate per county
-df = data15[['County', 'Graduation Rate', '% Severe Housing Problems']]
-bar_chart = alt.Chart(df).mark_bar(color='primary_color').encode(
-    x='County',
-    y='Graduation Rate'
-)
-# Create a line chart for % Rural using Altair
-line_chart = alt.Chart(df).mark_line(color='secondary_color').encode(
-    x='County',
-    y='% Severe Housing Problems'
-)
-# Overlay the two charts
-chart = (bar_chart + line_chart)
-st.altair_chart(chart, use_container_width=True)
+# read the selected CSV file based on the selected year
+data = pd.read_csv(files[selected_year - 2015])
+
+# create a bar chart based on the % of severe housing problems
+fig, ax = plt.subplots()
+ax.bar(data['County'], data['% of severe housing problems'])
+ax.set_xlabel("County")
+ax.set_ylabel("% of severe housing problems")
+ax.set_title("Severe Housing Problems in {}".format(selected_year))
+st.pyplot(fig)
 
 
 primary_color = "#bf293c"
