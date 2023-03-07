ab04ad412a866325357272916d5745bb106b64b4
Julia Crume <jcrume@unc.edu>
2023-03-07 13:28:53 -0500

    width bigger

--- pages/visualization.py
+++ pages/visualization.py
@@ -5,4 +5,4 @@ st.set_page_config(page_title="Sleep Visual", page_icon=":books:", layout="wide"
 
 st.header("Data Correlation Visualization")
 
-st.image("assets/Sleep vs. Grad.jpg", width = 300, height = 300)
+st.image("assets/Sleep vs. Grad.jpg", width = 500)
