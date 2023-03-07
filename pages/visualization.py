59cd96c8f21653c9f6e80bb51339e48573023f1d
Julia Crume <jcrume@unc.edu>
2023-03-07 13:24:12 -0500

    IMAGE

--- pages/visualization.py
+++ pages/visualization.py
@@ -5,23 +5,8 @@ st.set_page_config(page_title="Sleep Visual", page_icon=":books:", layout="wide"
 
 st.header("Data Correlation Visualization")
 
-st.image("Sleep vs. Grad.jpeg", width=100)
+uploaded_file = st.file_uploader("Sleep vs. Grad", type=[jpeg])
 
-primary_color = "#bf293c"
-secondary_color = "#3a3d7f"
-text_color = "#37393e"
-
-# Add CSS to customize text colors
-st.markdown(f"""
-    <style>
-        div.css-edivx2.e16fv1kl3 {{
-            color: {secondary_color};
-        }}
-        p {{
-            color: {text_color};
-        }}
-        h1, h2, h3, h4, h5, h6 {{
-            color: {primary_color};
-        }}
-    </style>
-""", unsafe_allow_html=True)
+if uploaded_file is not None:
+    # Display the uploaded image
+    st.image(uploaded_file, caption='Visual')
