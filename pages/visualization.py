2ee3b1793ac3b4674e0240ebb3f0968eec457c3b
Julia Crume <jcrume@unc.edu>
2023-03-07 13:26:54 -0500

    jpg

--- pages/visualization.py
+++ pages/visualization.py
@@ -5,6 +5,4 @@ st.set_page_config(page_title="Sleep Visual", page_icon=":books:", layout="wide"
 
 st.header("Data Correlation Visualization")
 
-uploaded_file = st.file_uploader("Sleep vs. Grad", type=[jpeg])
-
-st.image(uploaded_file, width = 200)
+st.image("assets/Sleep vs. Grad.jpg", width = 200)
