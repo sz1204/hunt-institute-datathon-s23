9911cd8971fae69ff8d94e93667310bb9c3ebd49
Julia Crume <jcrume@unc.edu>
2023-03-07 13:25:45 -0500

    widthhhh

--- pages/visualization.py
+++ pages/visualization.py
@@ -7,6 +7,4 @@ st.header("Data Correlation Visualization")
 
 uploaded_file = st.file_uploader("Sleep vs. Grad", type=[jpeg])
 
-if uploaded_file is not None:
-    # Display the uploaded image
-    st.image(uploaded_file, width = 200)
+st.image(uploaded_file, width = 200)
