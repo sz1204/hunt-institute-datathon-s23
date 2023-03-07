914977b6c3dd6462563667712427fcd628b8caf6
Julia Crume <jcrume@unc.edu>
2023-03-07 13:32:20 -0500

    updates

--- homepage.py
+++ homepage.py
@@ -30,7 +30,7 @@ st.set_page_config(page_title="Homepage", page_icon=":books:", layout="wide")
 st.image("assets/hunt-institute.jpg", width=100)
 
 st.title("Welcome to the Steminists' Hunt Institute Datathon 2023 dashboard!")
-st.markdown("Our project focused on measuring how certain legislation affecting affordable housing and youth mental health impacted student physical health, mental health, and graduation.")
+st.markdown("Our project focused on measuring how amount and quality of sleep can impact physical health, mental health, and graduation.")
 st.markdown('   ')
 
 
