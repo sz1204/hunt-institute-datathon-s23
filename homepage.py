748e94d008ddcfd5c97fde5511e177612b73d13e
Sarah Zhang <sz1204@ad.unc.edu>
2023-03-07 21:20:49 -0500

    final edits to datathon proj

--- homepage.py
+++ homepage.py
@@ -29,7 +29,7 @@ st.set_page_config(page_title="Homepage", page_icon=":books:", layout="wide")
 st.image("assets/hunt-institute.jpg", width=100)
 
 st.title("Welcome to the Steminists' Hunt Institute Datathon 2023 dashboard!")
-st.markdown("Our project focused on measuring how certain legislation affecting affordable housing and youth mental health impacted student physical health, mental health, and graduation.")
+st.markdown("Our project focused on the correlation between low sleep levels and high school graduation levels.")
 st.markdown('   ')
 
 
